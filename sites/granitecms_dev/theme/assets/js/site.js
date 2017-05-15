b64f4535c58dea8278216aade51417d67974d3da
Evan Smith <esincork@gmail.com>
2017-05-15 11:15:32 +0100

    Add a nice search box and VueJS

--- sites/granitecms_dev/theme/assets/js/site.js
+++ sites/granitecms_dev/theme/assets/js/site.js
@@ -1,88 +1,22 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    searchInput: ""
+  },
+  methods: {
+  	search: function(event){
+  		event.preventDefault();
 
-$(function() {
-
-
-	$('#bannerSlider').slick({
-	  dots: false,
-	  autoplay: true,
-	  autoplaySpeed: 1500,
-	  infinite: true,
-	  speed: 500,
-	  fade: true,
-	  cssEase: 'linear'
-	});
-
-
-
-	//scrolls smoothly to next section
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-					return false;
-				}
-			}
-	});
-
-	//show/hide main nav
-	$( ".toggleMainNav" ).click(function() {
-		$( ".mainNav" ).toggleClass( "open" );
-	});
-
-	//show/hide page nav
-	$( ".togglePageNav" ).click(function() {
-		$( ".sidebar" ).toggleClass( "open" );
-	});
-
-	//show/hide tabs
-	$( ".toggleTabs" ).click(function() {
-		$( ".tab-container" ).toggleClass( "open" );
-	});
-	$( ".etabs a" ).click(function() {
-		$( ".tab-container" ).toggleClass( "open" );
-	});
-
-	//change appearange of sticky top nav bar
-	var shrinkHeader = 50;
-		$(window).scroll(function() {
-			var scroll = getCurrentScroll();
-			if ( scroll >= shrinkHeader ) {
-				$('.header.container').addClass('sticky');
-			}
-			else {
-				$('.header.container').removeClass('sticky');
-			}
-		});
-	function getCurrentScroll() {
-		return window.pageYOffset;
-	}
-
-	//easy tabs
-	$('.tab-container').easytabs();
-
-	//accordion
-	var allPanels = $('.accordion > div').hide();
-	$('.accordion > h3 > a').click(function() {
-		$this = $(this);
-			$target =  $this.parent().next().slideToggle();
-			$target.toggleClass('open');
-			$this.parent().toggleClass('open');
-			return false;
-	});
-
-});
-
-function closeCookiePolicy(){
-	var url = "/cookie-policy";
-	$.ajax({
-		url: url,
-		asynchronous:true,
-		success:function(response){
-			$("#stickycookiePolicy").fadeOut();
-		}
-	});
-}
+  		if( this.searchInput !== "" ){
+	  		$.ajax({
+			  url: "/api/search/"+this.searchInput,
+			  method: "GET",
+			  dataType: "json",
+			  success: function(result){
+			  	console.log(result);
+			  }
+			});
+  		}
+  	}
+  }
+})
--- sites/granitecms_dev/theme/views/layouts/header.blade.php
+++ sites/granitecms_dev/theme/views/layouts/header.blade.php
@@ -11,31 +11,8 @@
 
 	@yield('extra-css')
 
-	<link rel="stylesheet" type="text/css" href="{{ siteAsset('css/slick/slick.css') }}"/>
-	
-
-	<script src="{{ siteAsset('js/jquery/jquery-1.11.2.min.js') }}"></script>
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+	<script src="https://unpkg.com/vue@2.3.3"></script>
 
 </head>
 <body id="@yield('page-id')">
-
-@include('granitecms_dev.theme.views.partials.cookie-policy')
-	<div id="wrap">
-		<!-- ********** HEADER ********** -->
-		<div class="container header">
-			<div class="row">
-				<div class="col span4">
-					<h1><a href="/" title="DefaultSite">DefaultSite</a></h1>
-				</div><!--/col-->
-				<div class="col span8">
-					<div class="contact">
-						<span><i class="fa fa-map-marker"></i><a href="/" class="" id="" title="GRANITE, Address, Address"   >GRANITE, Address, Address</a></span>
-						<span><i class="fa fa-phone"></i><a href="tel:+353212427890">+353 21 242 7890</a></span>
-						<span><i class="fa fa-envelope"></i><a href="mailto:infoto@default.ie">infoto@default.ie</a></span>
-					</div>
-				</div><!--/col-->
-				<div class="mainNav col span8">
-					@include('granitecms_dev.theme.views.partials.main-menu')
-				</div><!--/col-->
-			</div><!--/row-->
-		</div><!--/header-->

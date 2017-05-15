2f86cd46ee6f910bad2de798ef40d13483e8d8d7
Evan Smith <esincork@gmail.com>
2017-05-15 11:16:52 +0100

    Get site data api function

--- sites/granitecms_dev/theme/controllers/SiteController.php
+++ sites/granitecms_dev/theme/controllers/SiteController.php
@@ -3,32 +3,14 @@
 namespace Sites\granitecms_dev\theme\controllers;
 
 use App\Http\Controllers\Controller;
-// use Auth;
-use App\Page;
-use DB;
-use Illuminate\Http\Request;
+use Sites\granitecms_dev\theme\Site;
 
 class SiteController extends Controller
 {
-    public function index(Request $request)
+    public function batchGetSites($ids)
     {
-        $homepage_banners = DB::select("SELECT * FROM homepage_banners WHERE site = 1 ORDER BY display_order ASC");
+        $sites = Site::whereIn('id', $ids)->get();
 
-        return view('granitecms_dev.theme.views.home', ['banners' => $homepage_banners]);
-    }
-
-    public function pages($page_slug)
-    {
-        $page = Page::where('page_slug', $page_slug)->where('page_status', 'published')->first();
-        if ($page == null) {return abort('404');}
-
-        return view('granitecms_dev.theme.views.pages-default', ['page' => $page]);
-    }
-
-    public function cookiePolicy(Request $request)
-    {
-        return response()->json([
-            'status' => '200',
-        ]);
+        return apiResponse(SUCCESS, $sites);
     }
 }

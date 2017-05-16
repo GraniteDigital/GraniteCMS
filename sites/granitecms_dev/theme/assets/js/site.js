4d0e31d5ea0752d26ac0c9515fcac80b85ac18d3
Evan Smith <esincork@gmail.com>
2017-05-16 12:32:26 +0100

    Begin pulling data from API

--- sites/granitecms_dev/theme/assets/js/site.js
+++ sites/granitecms_dev/theme/assets/js/site.js
@@ -1,22 +1,37 @@
 var app = new Vue({
   el: '#app',
   data: {
-    searchInput: ""
+    searchInput: "",
+    sites: [],
+    currentSiteCounter: 0,
+
+
+    siteCounterStep: 20,
+    siteCounterBase: 0,
   },
   methods: {
   	search: function(event){
   		event.preventDefault();
+      this.currentSiteCounter = 0;
 
   		if( this.searchInput !== "" ){
 	  		$.ajax({
-			  url: "/api/search/"+this.searchInput,
-			  method: "GET",
-			  dataType: "json",
-			  success: function(result){
-			  	console.log(result);
-			  }
-			});
+          url: "/api/search/"+this.searchInput,
+          method: "GET",
+          dataType: "json",
+          success: function(result){
+            this.sites = result.data;
+            this.renderSites( this.siteCounterBase, this.siteCounterBase + this.siteCounterStep );
+          }
+        });
   		}
-  	}
+  	},
+
+    renderSites: function( lower, upper ){
+
+
+
+      this.currentSiteCounter = this.currentSiteCounter + this.siteCounterStep;
+    }
   }
 })

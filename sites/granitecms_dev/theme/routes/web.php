0cfd61430740fd7ef1a79b8fd3cc689450311bae
Evan Smith <esincork@gmail.com>
2017-05-19 18:05:33 +0100

    Remove commented out code

--- sites/granitecms_dev/theme/routes/web.php
+++ sites/granitecms_dev/theme/routes/web.php
@@ -11,10 +11,6 @@
 |
  */
 
-// Route::get('/', function () {
-//     return view('granitecms_dev.theme.views.home');
-// });
-
 Route::get('/', function () {
     return view('granitecms_dev.theme.views.home');
 });

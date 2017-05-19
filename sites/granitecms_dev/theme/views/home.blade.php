529faa11956ec07d69635cd0c7c5db174a96d58c
Evan Smith <esincork@gmail.com>
2017-05-19 18:05:58 +0100

    Vue template for site info

--- sites/granitecms_dev/theme/views/home.blade.php
+++ sites/granitecms_dev/theme/views/home.blade.php
@@ -4,10 +4,27 @@
 <div id="app">
 	
 	<form class="form-wrapper cf">
-	  	<input type="text" placeholder="Search here..." required v-model="searchInput">
 		<button type="submit" v-on:click="search">Search</button>
+	  	<input type="text" placeholder="Search here..." required v-model="searchInput">
 	</form>
-	@{{searchInput}}
+
+	<div v-for="item in sites">
+
+		<div class="left-image">
+
+		</div>
+		<div class="right-info">
+			<a v-bind:href="[ item.url ]" target="_BLANK"><h2>@{{ item.name }}</h2></a>
+			<a v-bind:href="[ item.url ]" target="_BLANK">Link</a>
+			<p>
+				<strong>Developers: </strong> @{{ item.developers }}
+			</p>
+
+			<p>
+				<strong>Project Managers: </strong> @{{ item.project_managers }}
+			</p>
+		</div>
+	</div>
 </div>
 @stop
 

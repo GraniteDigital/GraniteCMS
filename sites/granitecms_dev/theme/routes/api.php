36916b5eebe027f229a861f342ae8c5be2e54cb0
Evan Smith <esincork@gmail.com>
2017-05-19 18:05:26 +0100

    add site info API endpoint

--- sites/granitecms_dev/theme/routes/api.php
+++ sites/granitecms_dev/theme/routes/api.php
@@ -8,3 +8,5 @@ $tagController = new TagController();
 Route::get('/search/{tags}', function (Request $request, $tags) use ($tagController) {
     return $tagController->search($tags);
 });
+
+Route::get('/get_site_info/{ids}', 'SiteController@batchGetSites');
